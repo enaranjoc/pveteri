f218e1012cfa7396981a566f43fbf53fdfd16ab7
Erick <ariel99.en@gmail.com>
2022-09-05 01:44:48 -0500

    Tabla servicio

--- producto/admin.py
+++ producto/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from .models import Producto
+from .models import Marca, Producto, Servicio
 # Register your models here.
 
 class MarcaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'Nombre', 'Estado')
+    list_display = ('id', 'nombre', 'estado')
 
 class ProductoAdmin(admin.ModelAdmin):
-     list_display = ('id','Prod_nombre','Prod_marca','Prod_descripcion','Prod_stock','Prod_precio','Prod_estado')
+     list_display = ('id','nombre','marca','descripcion','stock','precio','estado')
 
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre','descripcion','precio', 'estado')
 
 admin.site.register(Marca, MarcaAdmin)
 admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Servicio, ServicioAdmin)
 
 
--- producto/models.py
+++ producto/models.py
@@ -8,15 +8,24 @@ class Marca(models.Model):
     
     def __str__(self):
         return self.nombre
+
 # Tabla producto
 class Producto(models.Model):
     nombre = models.CharField(max_length=200, unique=True)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    slug = models.CharField(max_length=200, unique=True)
-    Prod_descripcion = models.TextField(max_length=500, blank=True)
-    Prod_stock = models.IntegerField()
-    Prod_precio = models.IntegerField()
-    Prod_estado = models.BooleanField(default=True)
+    descripcion = models.TextField(max_length=500, blank=True)
+    stock = models.IntegerField()
+    precio = models.IntegerField()
+    estado = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.Prod_nombre
+        return self.nombre
+
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=200, unique=True)
+    descripcion = models.TextField(max_length=500, blank=True)
+    precio = models.IntegerField()
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
