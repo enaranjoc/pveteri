42be29e168a0806f76d76cb43fc6e03bde5ba695
Erick <ariel99.en@gmail.com>
2022-09-05 00:01:36 -0500

    Coneccion con base de datos

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'veterinaria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        # Configuracion para trabajar con posgreSql
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'veterinaria', 
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'PORT': '5432',
     }
 }
 
