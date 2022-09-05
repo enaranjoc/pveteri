1ae83ec4596e0d54c6992b10b969c9cb7cffba1b
Erick <ariel99.en@gmail.com>
2022-09-05 01:11:26 -0500

    Creacion del modelo producto

--- producto/admin.py
+++ producto/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Producto
 # Register your models here.
+
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Nombre', 'Estado')
+
+class ProductoAdmin(admin.ModelAdmin):
+     list_display = ('id','Prod_nombre','Prod_marca','Prod_descripcion','Prod_stock','Prod_precio','Prod_estado')
+
+
+admin.site.register(Marca, MarcaAdmin)
+admin.site.register(Producto, ProductoAdmin)
+
+
--- producto/models.py
+++ producto/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# Tabla marca
 class Marca(models.Model):
     nombre = models.CharField(max_length=200, unique=True)
     estado = models.BooleanField(default=True)
     
     def __str__(self):
-        return self.Mar_nombre
+        return self.nombre
+# Tabla producto
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200, unique=True)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    slug = models.CharField(max_length=200, unique=True)
+    Prod_descripcion = models.TextField(max_length=500, blank=True)
+    Prod_stock = models.IntegerField()
+    Prod_precio = models.IntegerField()
+    Prod_estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.Prod_nombre
